66c1809973e3c98ce2c8bc840d889171ed6a993d
Ken Brittain <ken.brittain@hotmail.com>
2022-06-17 06:53:11 -0400

    Allow for adding runbook names
    The runbook names are the root of the name, minus the version number,
    and used to load runbook at runtime.

--- compiler/Runbook/Models/PackageModel.cs
+++ compiler/Runbook/Models/PackageModel.cs
@@ -5,6 +5,26 @@ namespace Runbook.Models;
 /// </summary>
 public class PackageModel : StandardModel
 {
-    
+    private readonly List<string> _runbooks;
+
+    public PackageModel()
+    {
+	_runbooks = new List<string>();
+    }
+
+    /// <summary>
+    /// Append the <c>runbooks</c> section with the specified name.
+    /// </summary>
+    /// <param name="runbookName">Root name of the runbook.</param>
+    /// <exception cref="Argumentexception">Thrown when the name if <c>null</c> or empty.</exception>
+    public void AddRunbook(string  runbookName)
+    {
+	if (runbookName == null || runbookName == string.Empty)
+	{
+	    throw new ArgumentException($"The runbook name must contain a value!", nameof(runbookName));
+	}
+	
+	_runbooks.Add(runbookName);
+    }
 }
 
